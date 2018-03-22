2d65b91ab50b356d6fa209c2f5adda63d160cb1a
jackHay22 <jjphay@gmail.com>
2018-03-22 20:41:30 +0000

    more additions to tile engine

--- src/upstream/tilemap/tiles.clj
+++ src/upstream/tilemap/tiles.clj
@@ -24,6 +24,20 @@
     )
   )
 
+(defn parse-map-file
+  "resource path, list of keywords for storing the game map as a list of maps (i.e. '(:image :sound)
+  or '(:image :sound :height :blocked?))"
+  [path fields]
+  (map (fn [line]
+      (map #(Integer. %)
+        (map #(clojure.string/split % #",")
+        (clojure.string/split line #" ")))
+      (clojure.string/split-lines (slurp path)))))
+
+(defn get-tile
+  "given (loaded map), get x,y tile"
+  [loaded-map x y])
+
 (defn split-master
   "split master image into list of images (1 dimensional)"
   [load-fn tiles-across tiles-down tile-width tile-height]
