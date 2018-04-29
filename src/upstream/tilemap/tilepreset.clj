d702403e260dfb1be2cf1164cf7fea01d8311b2d
jackHay22 <jjphay@gmail.com>
2018-04-29 21:09:51 +0100

    lighting transparency bug fix

--- src/upstream/tilemap/tilepreset.clj
+++ src/upstream/tilemap/tilepreset.clj
@@ -30,11 +30,11 @@
                     (TileResource. "tiles/poc_sawmill.png" 170 60 1797 1122))
        :map-attributes (list :image-index :blocked? :height :sound)})
 
-(def layer-1-lighting-opacity 100)
-(def layer-2-lighting-opacity 120)
+(def layer-1-lighting-opacity 200)
+(def layer-2-lighting-opacity 100)
 
-(def layer-1-lighting-radius (float 800.0))
-(def layer-2-lighting-radius (float 950.0))
+(def layer-1-lighting-radius (float 1000.0))
+(def layer-2-lighting-radius (float 800.0))
 
 (defn layer-1-rgb
   [x y] {:color (Color. 0 0 0 layer-1-lighting-opacity) :radius layer-1-lighting-radius})
--- src/upstream/utilities/lighting.clj
+++ src/upstream/utilities/lighting.clj
@@ -8,6 +8,7 @@
 (import java.awt.RadialGradientPaint)
 (import java.awt.Graphics2D)
 (import java.awt.AlphaComposite)
+(import java.awt.image.BufferedImage)
 
 (defn get-lighting-profile
   "returns color given a tile and a layer"
@@ -17,12 +18,16 @@
 (defn render-lighting
   "render lighting at point"
   [gr x y layer]
-  (let [g2d (cast Graphics2D gr)
+  (let [win-width @config/WINDOW-RESOURCE-WIDTH
+        win-height @config/WINDOW-RESOURCE-HEIGHT
+        lighting-layer (BufferedImage. win-width win-height BufferedImage/TYPE_INT_ARGB)
+        g2d (cast Graphics2D (.createGraphics lighting-layer))
         dist (float-array [0.1 1.0])
         lighting-profile (get-lighting-profile layer x y)
         radial-color (into-array Color [(Color. 0.0 0.0 0.0 0.0) (:color lighting-profile)])
         gradient (RadialGradientPaint. (float x) (float y) (:radius lighting-profile) dist radial-color)]
         (.setPaint g2d gradient)
         (.setComposite g2d (AlphaComposite/getInstance AlphaComposite/SRC_OVER 0.95))
-        (.fillRect g2d 0 0 @config/WINDOW-RESOURCE-WIDTH @config/WINDOW-RESOURCE-HEIGHT)))
-        ;TODO: confirm that not disposing g2d is okay
+        (.fillRect g2d 0 0 win-width win-height)
+        (.drawImage gr lighting-layer 0 0 win-width win-height nil)
+        (.dispose g2d)))
