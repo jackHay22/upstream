b7b5968d8b9781db69bd657c1365155a2193895c
jackHay22 <jjphay@gmail.com>
2018-05-01 11:23:23 +0100

    change entity decision implementation system using macros

--- src/upstream/entities/decisionlib.clj
+++ src/upstream/entities/decisionlib.clj
@@ -2,28 +2,16 @@
   (:require [upstream.config :as config])
   (:gen-class))
 
-(def prefix-mappings
-  {})
+(defmacro defpredicate
+  "Macro for defining decision functions"
+  [name operation]
+  (list 'def (str "_" name "_")
+        (list 'fn '[entity-context]
+                  (list operation 'entity-context))))
 
-;Note: all predicates take their entity context
-;All actions take the current result map and their entity context
-
-(def decision-function-mappings
-  {:action {:prefix-1 {}}
-   :predicate {:prefix-1 {:and :and}}})
-
-(defn get-decision-function
-  "take predicate or action and return function"
-  [statement-type identifier]
-  (get
-    (get
-      (statement-type decision-function-mappings)
-      (first identifier))
-    (second identifier)))
-
-(def get-decision-function-cached (memoize get-decision-function))
-
-(defn add-lookup-prefix
-  "for each action, add a lookup prefix for more efficient search"
-  [symbol]
-  (list (symbol prefix-mappings) symbol))
+(defmacro defaction
+  "Macro for defining decision functions"
+  [name operation]
+  (list 'def (str "_" name "_")
+      (list 'fn '[res-map entity-context]
+              (list operation 'res-map 'entity-context))))
--- src/upstream/entities/entitydecisionmanager.clj
+++ src/upstream/entities/entitydecisionmanager.clj
@@ -7,44 +7,38 @@
 
 (def standard-result-map {:update-facing nil :update-action nil})
 
-(defn append-lookup-symbols
-  "add lookup args to statement lines"
-  [loaded-statements]
-  ;TODO
-  (map (fn [statement-line] ) loaded-statements))
-
 (defn load-entity-decisions
   "load decisions from file"
   [file]
   (if file
     ;TODO: add prefixes to loaded decisions (predicates and actions)
-    (append-lookup-symbols
-        (with-open [reader (clojure.java.io/reader (io/resource file))]
+    ; TODO: load as decisions
+    (with-open [reader (clojure.java.io/reader (io/resource file))]
             (map (fn [instr-pair]
                       (map (fn [instr]
                         (map #(read-string %) (clojure.string/split instr #" "))) instr-pair))
                 (map (fn [line] (clojure.string/split line #" | "))
-                    (clojure.string/split-lines (clojure.string/join "\n" (line-seq reader)))))))
+                    (clojure.string/split-lines (clojure.string/join "\n" (line-seq reader))))))
     false))
 
-(defn evaluate-actions
-  "perform action and return result"
-  [actions entity-context]
-  (reduce #(%2 %1 entity-context) standard-result-map
-          (map #(decisionlib/get-decision-function-cached :action %) actions)))
+; (defn evaluate-actions
+;   "perform action and return result"
+;   [actions entity-context]
+;   (reduce #(%2 %1 entity-context) standard-result-map
+;           (map #(decisionlib/get-decision-function-cached :action %) actions)))
 
-(defn evaluate-predicates
-  "take predicates and evaluate"
-  [preds entity-context]
-  (let [resolve-symbols (map #(decisionlib/get-decision-function-cached :predicate %) preds)]
-      (reduce #((first resolve-symbols) %1 (%2 entity-context)) true (rest resolve-symbols))))
+; (defn evaluate-predicates
+;   "take predicates and evaluate"
+;   [preds entity-context]
+;   (let [resolve-symbols (map #(decisionlib/get-decision-function-cached :predicate %) preds)]
+;       (reduce #((first resolve-symbols) %1 (%2 entity-context)) true (rest resolve-symbols))))
 
-(defn make-player-decision
-  "take loaded decisions and operate on first applicable
-  --format: ((and/or & :preds) (& :actions))
-  --context has :all-positions"
-  [entity-context]
-  (evaluate-actions
-      (reduce (fn [res statement] (if (evaluate-predicates (first statement) entity-context)
-                                      (reduced (second statement)) res))
-      false (:decisions entity-context)) entity-context))
+; (defn make-player-decision
+;   "take loaded decisions and operate on first applicable
+;   --format: ((and/or & :preds) (& :actions))
+;   --context has :all-positions"
+;   [entity-context]
+;   (evaluate-actions
+;       (reduce (fn [res statement] (if (evaluate-predicates (first statement) entity-context)
+;                                       (reduced (second statement)) res))
+;       false (:decisions entity-context)) entity-context))
